6a49d46e0a5fc8aa540675fca06f8a1815425364
LiiLk <khalil.abbioui@gmail.com>
2021-09-23 11:35:48 +0200

    Version Final V1

--- nvidia/src/Controller/HomeController.php
+++ nvidia/src/Controller/HomeController.php
@@ -2,13 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use App\Form\ProduitFormType;
 use App\Repository\ProduitRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+use PhpParser\Builder\Property;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $repository;
+    /**
+     * @var ObjectManager
+     */
+    private $em;
+
+    public function __construct(ProduitRepository $repository, EntityManagerInterface $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+
     /**
      * @Route("/accueil", name="accueil")
      */
@@ -20,9 +40,9 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index(ProduitRepository $repository): Response
+    public function index()
     {
-        $produits = $repository->findAll();
+        $produits = $this->repository->findAll();
 
         return $this->render('home/index.html.twig', [
             'produits' => $produits,
@@ -30,10 +50,59 @@ class HomeController extends AbstractController
     }
 
     /**
+     * @Route("/ajoutproduit", name="ajoutproduit")
+     */
+    public function ajout(Request $request){
+        $produit = new Produit();
+        $form = $this->createForm(ProduitFormType::class, $produit);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $this->em->persist($produit);
+            $this->em->flush();
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('home/produitAjout.html.twig', [
+            'produit' => $produit,
+            'form' => $form->createView()
+        ]);
+    }
+    /**
+     * @Route("/editproduit/{id}", name="editproduit", methods="GET|POST")
+     */
+    public function edit(Produit $produit, Request $request){
+        $form = $this->createForm(ProduitFormType::class, $produit);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $this->em->flush();
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('home/produitEdit.html.twig', [
+            'produit' => $produit,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/supprimerproduit/{id}", name="supprimerproduit")
+     * @param Produit $produit
+     * @return RedirectResponse
+     */
+    public function supprimer(Produit $produit, Request $request){
+        if ($produit) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($produit);
+            $em->flush();
+        }
+        return $this->redirectToRoute('home');
+    }
+
+
+    /**
      * @Route("/details/{id}", name="details")
      */
-    public function details($id, ProduitRepository $repository)  {
-        $produitsDetail = $repository->findBy(['id' => $id]);
+    public function details($id)  {
+        $produitsDetail = $this->repository->findBy(['id' => $id]);
 
         return $this->render('home/produitDetail.html.twig',['detailsProduit' => $produitsDetail]);
     }
@@ -41,8 +110,8 @@ class HomeController extends AbstractController
     /**
      * @Route("/news/{id}", name="news")
      */
-    public function news($id, ProduitRepository $repository)  {
-        $produitsDetail = $repository->findBy(['id' => $id]);
+    public function news($id)  {
+        $produitsDetail = $this->repository->findBy(['id' => $id]);
 
         return $this->render('home/news.html.twig',['detailsProduit' => $produitsDetail]);
     }
